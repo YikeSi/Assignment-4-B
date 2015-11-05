538cd0704a5d0a12e51b8a6021c5369dc2617a40
YikeSi <si.y@husky.neu.edu>
2015-11-05 17:37:21 -0500

    v

--- script.js
+++ script.js
@@ -35,25 +35,99 @@ plot.append('g').attr('class','axis axis-x')
 plot.append('g').attr('class','axis axis-y')
     .call(axisY);
 
+
+
 //Start importing data
-d3.csv('/data/world_bank_2012.csv', parse, dataLoaded);
+d3.csv('data/fao_combined_world_1963_2013.csv', parse, dataLoaded);
 
 function parse(d){
 
-    //Eliminate records for which gdp per capita isn't available
-
-    //Check "primary completion" and "urban population" columns
-    //if figure is unavailable and denoted as "..", replace it with undefined
-    //otherwise, parse the figure into numbers
     return {
-
+        item: d.ItemName,
+        year: +d.Year,
+        value: +d.Value
     };
 
+    console.log(d);
 
 
-}
-
-function dataLoaded(error, rows){
 
 }
+
+function dataLoaded(err, data){
+
+    //nest data
+    var nestedData = d3.nest()
+        .key(function(d){return d.item})
+        .entries(data);
+    console.log(nestedData);
+
+    var line = d3.svg.line()
+        .x(function(d){return scaleX(d.year)})
+        .y(function(d){return scaleY(d.value)})
+
+    //tea-line
+    plot.append('path')
+        .datum(nestedData[0].values)
+        .attr('class','tea-data-line data-line')
+        .attr('d',line)
+
+
+    //coffee-line
+    plot.append('path')
+        .datum(nestedData[1].values)
+        .attr('class','coffee-data-line data-line')
+        .attr('d',line)
+
+    //circles and tooltips
+    plot.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('class','data-point')
+        .attr('cx',function(d){return scaleX(d.year)})
+        .attr('cy',function(d){return scaleY(d.value)})
+        .attr('r',5)
+        .on('mouseover',function(d){
+            var tip = d3.select('.custom-tooltip').style('opacity',1)
+                tip.select('#item').html(d.item)
+                tip.select('#year').html(d.year)
+                tip.select('#value').html(d.value)
+            console.log(tip)
+
+        })
+        .on('mousemove',function(){
+            var xy = d3.mouse(canvas.node());
+            var tooltip = d3.select('.custom-tooltip')
+            tooltip
+                .transition()
+                .duration(300)
+                .style('left',xy[0]+20+'px')
+                .style('top',(xy[1]+20)+'px');
+
+        })
+        .on('mouseleave',function(){
+            var tooltip = d3.select('.custom-tooltip')
+                .transition()
+                .style('opacity',0)
+        })
+
+    //plot.append('path')
+    //    .attr('class','tea-data-line')
+    //    .datum(nestedData[0])
+    //    .attr('d',line)
+
+    //var nodeenter = plot.selectAll('circle')
+    //                        .data(data)
+    //                        .enter()
+    //    nodeenter.append('g')
+    //        .append('circle')
+    //        .attr('class','tea')
+    //        .attr('cx',function(d){return scaleX(d.year)})
+    //        .attr('cy',function(d){return scaleX(d.value)})
+    //        .attr('r',5)
+    //        .style('fill-opacity',.1)
+
+    }
+
 
